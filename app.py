661d3385bc6fe9c94d8fa2357a44190765f90352
Khushi Jain <khushij2312@gmail.com>
2026-06-16 18:08:55 +0530

    Initial commit

--- app.py
+++ app.py
@@ -10,4 +10,10 @@ def home():
 def videos():
     return render_template("videos.html")
 
-app.run(debug=True)
+app.run()
+
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
